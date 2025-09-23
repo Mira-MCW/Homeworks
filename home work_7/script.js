b1113ddeb35c90913fa407e778b52cd62f788f9a
Mira M.C.W. <mira.cyber.witch@gmail.com>
2025-09-23 19:57:47 +0200

    Add files via upload

---
+++ home work_7/script.js
@@ -0,0 +1,38 @@
+
+//Задания 1. 
+
+//Объявите две переменные firstName и lastName и присвойте им ваши имя и фамилию.
+
+//С помощью конкатенации строк создайте третью переменную greeting, содержащую приветствие в формате "Привет, Имя Фамилия!".
+
+//Выведите переменную greeting в консоль.
+
+
+let firstName = "Mira";
+let lastName = "Mironovska";
+let greeting = "Ciao " + firstName + " " + lastName + "!";
+console.log (greeting);
+
+// Вывод:
+
+Ciao Mira Mironovska!
+
+//Задания 2.*
+
+//Создайте переменную sentencePart1 и присвойте ей начало предложения (например, "Я люблю").
+
+//Создайте переменную sentencePart2 и присвойте ей завершение предложения (например, "программирование").
+
+//С помощью конкатенации строк создайте переменную completeSentence, объединяющую обе части предложения.
+
+//Выведите переменную completeSentence в консоль.
+
+
+let sentencePart1="I love";
+let sentencePart2="programming";
+let completeSentence=sentencePart1 + " " + sentencePart2;
+console.log(completeSentence);
+
+// Вывод:
+
+I love programming
